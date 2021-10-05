bf720f83c807e45e5df29d6378c84e6c6fb6d220
Doble-Ge <ginogutierrez95@gmail.com>
2021-10-04 21:20:14 -0300

    holis

---
+++ Back/src/index.js
@@ -0,0 +1,11 @@
+//import app from './app';
+
+/*async function main () {
+    await app.listen(3000);
+    console.log('Server on port 3000');
+};
+
+main();*/
+const hello = "hello world"
+
+console.log(hello)
