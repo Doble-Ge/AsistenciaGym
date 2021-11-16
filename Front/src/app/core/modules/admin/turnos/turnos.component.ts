73fe514097fdf791b762a3c18afcfc04ae7491df
Jose Gho <josegho001@gmail.com>
2021-11-16 14:12:25 -0300

    Probando funcion meses

--- Front/src/app/core/modules/admin/turnos/turnos.component.ts
+++ Front/src/app/core/modules/admin/turnos/turnos.component.ts
@@ -4,6 +4,7 @@ import {Cliente} from '../../../models/cliente'
 import {Router} from '@angular/router'
 import { ActivatedRoute } from '@angular/router';
 import { Turno } from 'src/app/core/models/turno';
+
 @Component({
   selector: 'app-turnos',
   templateUrl: './turnos.component.html',
@@ -14,7 +15,20 @@ export class TurnosComponent implements OnInit {
   turnoPorId!:Turno;
   turnoCliente!:Turno;
   turnosFebrero!:Turno[];
+  arrayDiasMes!:number[];
   arrayTurnosMeses=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+  arrayDiasEnero=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+  arrayDiasFebrero=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28];
+  arrayDiasMarzo=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+  arrayDiasAbril=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+  arrayDiasMayo=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+  arrayDiasJunio=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+  arrayDiasJulio=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+  arrayDiasAgosto=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+  arrayDiasSeptiembre=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+  arrayDiasOctubre=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+  arrayDiasNoviembre=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+  arrayDiasDiciembre=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
   constructor(private service:ApiServiceService,private routes:Router,private activeRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -22,14 +36,7 @@ export class TurnosComponent implements OnInit {
       this.arrayTurnos=turnos;
       console.log(this.arrayTurnos)
       //console.log(this.arrayDias)
-      this.turnosFebrero=this.arrayTurnos.filter((turnosMes)=>{
-        if(turnosMes.mes==2){
-          return true;
-        }
-        else{
-          return false;
-        }
-      })
+      
     })
   }
   revisarTurnos(turno: number){
@@ -45,10 +52,9 @@ export class TurnosComponent implements OnInit {
     let numMes:number;
     if(mes=='Enero'){
       numMes=1;
-      console.log("el mes es:"+mes+"numero"+numMes)
     }
     else if(mes=='Febrero'){
-      numMes=2;
+        numMes=2;   
     }
     else if(mes=='Marzo'){
       numMes=3;
@@ -81,7 +87,77 @@ export class TurnosComponent implements OnInit {
       numMes=12;
     }
 
-    console.log("mes: "+mes +" "+"numer: "+numMes)
+    //console.log("mes: "+mes +" "+"numer: "+numMes + "array1 "+this.turnosFebrero[0].mesDescripcion+" array2 "+this.turnosFebrero[1].horario)
+    //this.filtrarTurnosMeses(numMes);
   }
 
-}
+
+
+  filtrarDiasMeses(mes:string){
+    if(mes=='Enero'){
+      this.arrayDiasMes=this.arrayDiasEnero;
+      console.log(this.arrayDiasMes);
+    }
+    else if(mes=='Febrero'){
+      this.arrayDiasMes=this.arrayDiasFebrero;
+      console.log(this.arrayDiasMes);
+    }
+    else if(mes=='Marzo'){
+      this.arrayDiasMes=this.arrayDiasMarzo;
+      console.log(this.arrayDiasMes);
+    }
+    else if(mes=='Abril'){
+      this.arrayDiasMes=this.arrayDiasAbril;
+      console.log(this.arrayDiasMes);
+    }
+    else if(mes=='Mayo'){
+      this.arrayDiasMes=this.arrayDiasMayo;
+      console.log(this.arrayDiasMes);
+    }
+    else if(mes=='Junio'){
+      this.arrayDiasMes=this.arrayDiasJunio;
+      console.log(this.arrayDiasMes);
+    }
+    else if(mes=='Julio'){
+      this.arrayDiasMes=this.arrayDiasJulio;
+      console.log(this.arrayDiasMes);
+    }
+    else if(mes=='Agosto'){
+      this.arrayDiasMes=this.arrayDiasAgosto;
+      console.log(this.arrayDiasMes);
+    }
+    else if(mes=='Septiembre'){
+      this.arrayDiasMes=this.arrayDiasSeptiembre;
+      console.log(this.arrayDiasMes);
+    }
+    else if(mes=='Octubre'){
+      this.arrayDiasMes=this.arrayDiasOctubre;
+      console.log(this.arrayDiasMes);
+    }
+    else if(mes=='Noviembre'){
+      this.arrayDiasMes=this.arrayDiasNoviembre;
+      console.log(this.arrayDiasMes);
+    }
+    else{
+      this.arrayDiasMes=this.arrayDiasDiciembre;
+      console.log(this.arrayDiasMes);
+    }
+    
+  }
+  filtrarTurnosMeses(mes:number,dia:number){
+    let arrayMeses = this.arrayTurnos.filter((turnomes)=>{
+      if(turnomes.mes==mes){
+        let arrayMesesDias=arrayMeses.filter((turnosMesDia)=>{
+          
+        })
+        return true;
+      }
+      else{
+        console.log("No se encontraron turnos para el mes")
+        return false;
+      }
+    })
+  }
+  }
+
+
